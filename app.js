ffadf247931fbd60b4fbc5e4443032cba8778aa6
JKBUserTribe <jkb@usertribe.com>
2018-08-31 13:03:41 +0200

    Setup routes and DB
    Setup the user routes
    Connection to DB via mongoose with config file

---
+++ app.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const passport = require('passport');
+const mongoose = require('mongoose');
+const config = require('./config/database');
+
+// Connect to Database
+mongoose.connect(config.database, { useNewUrlParser: true });
+
+// On Connection to Database
+mongoose.connection.on('connected', () => {
+  console.log('Connected to database '+config.database);
+})
+
+// On Error to Database
+mongoose.connection.on('error', (err) => {
+  console.log('Database error '+err);
+})
+
+const app = express();
+
+const users = require('./routes/users');
+
+// Port Number
+const port = 3000;
+
+// CORS Middleware
+app.use(cors());
+
+// Set Static Folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Body Parser Middleware
+app.use(bodyParser.json());
+
+// User Profile path
+app.use('/users', users);
+
+// Index Route
+app.get('/', (req, res) => {
+  res.send('Invalid Endpoint');
+});
+
+// Start Server
+app.listen(port, () => {
+  console.log('Server start on port '+port);
+});
---
+++ config/database.js
@@ -0,0 +1,4 @@
+module.exports = {
+  database: 'mongodb://localhost:27017/udj',
+  secret: '4a2c3d7f10beebc0a22adf3e03d3c2e37fc361ba42858070dea7afe2dd8d2e85'
+}
