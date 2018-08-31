d720b1732f09c0522b836ab8780ed9e8b6147bdd
JKBUserTribe <jkb@usertribe.com>
2018-08-31 16:34:46 +0200

    Lock page with token
    test to lock out anyone without correct authorization

--- models/user.js
+++ models/user.js
@@ -36,7 +36,6 @@ module.exports.getUserByUsername = function(username, callback){
   User.findOne(query, callback);
 }
 
-
 module.exports.addUser = function(newUser, callback){
   bcrypt.genSalt(10, function(err, salt) {
     bcrypt.hash(newUser.password, salt, (err, hash) => {
