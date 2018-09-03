24dfb91a302a73b5afa88f6359b82f90d1c0cb9b
JKBUserTribe <jkb@usertribe.com>
2018-09-03 17:01:40 +0200

    logout
    button and event

--- angular-src/src/app/components/navbar/navbar.component.ts
+++ angular-src/src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../services/auth.service'
+import { FlashMessagesService } from 'angular2-flash-messages';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
@@ -7,9 +10,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private authService: AuthService,
+    private _flashMessagesService: FlashMessagesService,
+    private router: Router
+  ) { }
 
   ngOnInit() {
   }
 
+  onLogoutClick(){
+    this.authService.logout();
+    this._flashMessagesService.show('You have been successfully logged out', {
+      cssClass: 'alert-success',
+    });
+    this.router.navigate(['/'])
+    return false;
+  }
 }
